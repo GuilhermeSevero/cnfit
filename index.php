ff90654b53e5a27cc8ae52eecb4bf10bbc08dfe8
Guilherme Severo <guilhermehmsevero@hotmail.com>
2018-10-12 21:52:15 -0300

    commit inicial do projeto.

---
+++ index.php
@@ -0,0 +1,307 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>CNFIT - Vida Saudável</title>
+    <meta name="description" content="Página da CNFIT">
+    <meta name="keywords" content="Low Carb, fit, fitness, saúde, dieta">
+    
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700|Pinyon+Script" rel="stylesheet">
+    <link rel="stylesheet" href="css/styles-merged.css">
+    <link rel="stylesheet" href="css/style.min.css">
+
+    <!--[if lt IE 9]>
+      <script src="js/vendor/html5shiv.min.js"></script>
+      <script src="js/vendor/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    
+    <?php require_oncee('components/navbar.html')?>
+
+    <section class="flexslider" data-section="welcome">
+      <ul class="slides">
+        <li style="background-image: url(img/hero_bg_1.jpg)" class="overlay" data-stellar-background-ratio="0.5">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-8 col-md-offset-2">
+                <div class="probootstrap-slider-text text-center probootstrap-animate probootstrap-heading">
+                  <h1 class="primary-heading">Vida Saudável</h1>
+                  <h3 class="secondary-heading">CNFIT</h3>
+                  <p class="sub-heading">Seja Bem Vindo!</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </li>
+        <li style="background-image: url(img/hero_bg_2.jpg)" class="overlay">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-8 col-md-offset-2">
+                <div class="probootstrap-slider-text text-center probootstrap-animate probootstrap-heading">
+                  <h1 class="primary-heading">Vem Ser Saudável</h1>
+                  <h3 class="secondary-heading">Conosco</h3>
+                  <p class="sub-heading">Alguma coisa pra dizer sobre ser saudável</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          
+        </li>
+        <li style="background-image: url(img/hero_bg_3.jpg)" class="overlay">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-8 col-md-offset-2">
+                <div class="probootstrap-slider-text text-center probootstrap-animate probootstrap-heading">
+                  <h1 class="primary-heading">Aproveite</h1>
+                  <h3 class="secondary-heading">CNFIT</h3>
+                  <p class="sub-heading">Mais coisas, se quiser dizer...</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </section>
+
+    <section class="probootstrap-section probootstrap-bg-white">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-5 text-center probootstrap-animate">
+            <div class="probootstrap-heading dark">
+              <h1 class="primary-heading">Descubra</h1>
+              <h3 class="secondary-heading">Nossa História</h3>
+              <span class="seperator">* * *</span>
+            </div>
+            <p>Voluptatibus quaerat laboriosam fugit non Ut consequatur animi est molestiae enim a voluptate totam natus modi debitis 
+              dicta impedit voluptatum quod sapiente illo saepe explicabo quisquam perferendis labore et illum suscipit</p>
+            <p><a href="#" class="probootstrap-custom-link">About Us</a></p>
+          </div>
+          <div class="col-md-6 col-md-push-1 probootstrap-animate">
+            <p><img src="img/img_1.jpg" alt="Free Bootstrap Template by uicookies.com" class="img-responsive"></p>
+          </div>
+        </div>
+        <!-- END row -->
+      </div>
+    </section>
+
+    <section class="probootstrap-section-bg overlay" style="background-image: url(img/hero_bg_2.jpg);" data-stellar-background-ratio="0.5" data-section="specialties">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 text-center probootstrap-animate">
+            <div class="probootstrap-heading">
+              <h2 class="primary-heading">Descubra</h2>
+              <h3 class="secondary-heading">Nossas Especialidades</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+     <!-- probootstrap-bg-white -->
+    <section class="probootstrap-section">
+      <div class="container">
+        <div class="row">
+          <div class="probootstrap-cell-retro">
+            <div class="half">
+
+              <div class="probootstrap-cell probootstrap-animate" data-animate-effect="fadeIn">
+                <div class="image" style="background-image: url(img/img_square_1.jpg);"></div>
+                <div class="text text-center">
+                  <h3>Pudim Light</h3>
+                  <p>Laboriosam pariatur modi praesentium deleniti molestiae officiis atque numquam.</p>
+                  <p class="price">R$ 30,00</p>
+                </div>
+              </div>
+              <div class="probootstrap-cell reverse probootstrap-animate" data-animate-effect="fadeIn">
+                <div class="image" style="background-image: url(img/img_square_2.jpg);"></div>
+                <div class="text text-center">
+                  <h3>Mousse de Morango</h3>
+                  <p>Laboriosam pariatur modi praesentium deleniti molestiae officiis atque numquam.</p>
+                  <p class="price">R$ 10,00</p>
+                </div>
+              </div>
+              <div class="probootstrap-cell probootstrap-animate" data-animate-effect="fadeIn">
+                <div class="image" style="background-image: url(img/img_square_3.jpg);"></div>
+                <div class="text text-center">
+                  <h3>Mousse de Maracujá</h3>
+                  <p>Laboriosam pariatur modi praesentium deleniti molestiae officiis atque numquam.</p>
+                  <p class="price">R$ 10,00</p>
+                </div>
+              </div>
+
+            </div>
+            <div class="half">
+
+              <div class="probootstrap-cell probootstrap-animate" data-animate-effect="fadeIn">
+                <div class="image" style="background-image: url(img/img_square_4.jpg);"></div>
+                <div class="text text-center">
+                  <h3>Croquete de Carne</h3>
+                  <p>Laboriosam pariatur modi praesentium deleniti molestiae officiis atque numquam.</p>
+                  <p class="price">R$ 8,00</p>
+                </div>
+              </div>
+              <div class="probootstrap-cell reverse probootstrap-animate" data-animate-effect="fadeIn">
+                <div class="image" style="background-image: url(img/img_square_5.jpg);"></div>
+                <div class="text text-center">
+                  <h3>Coxinha de Frango</h3>
+                  <p>Laboriosam pariatur modi praesentium deleniti molestiae officiis atque numquam.</p>
+                  <p class="price">R$ 8,00</p>
+                </div>
+              </div>
+              <!-- <div class="probootstrap-cell probootstrap-animate" data-animate-effect="fadeIn">
+                <div class="image" style="background-image: url(img/img_square_1.jpg);"></div>
+                <div class="text text-center">
+                  <h3>Fried Potatoes with Garlic</h3>
+                  <p>Laboriosam pariatur modi praesentium deleniti molestiae officiis atque numquam.</p>
+                  <p class="price">$32.99</p>
+                </div>
+              </div> -->
+              
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="probootstrap-section-bg overlay" style="background-image: url(img/hero_bg_3.jpg);"  data-stellar-background-ratio="0.5"  data-section="menu">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 text-center probootstrap-animate">
+            <div class="probootstrap-heading">
+              <h2 class="primary-heading">Descubra</h2>
+              <h3 class="secondary-heading">Nossos Pratos</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="probootstrap-section probootstrap-bg-white">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <ul class="menus">
+              <li>
+                <figure class="image"><img src="img/img_square_1.jpg" alt="Free Bootstrap Template by uicookies.com"></figure>
+                <div class="text">
+                  <span class="price">R$ 30.00</span>
+                  <h3>Pudim Light</h3>
+                  <p>Crab / Potatoes / Rice</p>
+                </div>
+              </li>
+              <li>
+                <figure class="image"><img src="img/img_square_2.jpg" alt="Free Bootstrap Template by uicookies.com"></figure>
+                <div class="text">
+                  <span class="price">R$ 10,00</span>
+                  <h3>Mousse de Morango</h3>
+                  <p>Crab / Potatoes / Rice</p>
+                </div>
+              </li>
+              <li>
+                <figure class="image"><img src="img/img_square_3.jpg" alt="Free Bootstrap Template by uicookies.com"></figure>
+                <div class="text">
+                  <span class="price">R$ 10,00</span>
+                  <h3>Mousse de Maracujá</h3>
+                  <p>Crab / Potatoes / Rice</p>
+                </div>
+              </li>
+              <li>
+                <figure class="image"><img src="img/img_square_4.jpg" alt="Free Bootstrap Template by uicookies.com"></figure>
+                <div class="text">
+                  <span class="price">R$ 8,00</span>
+                  <h3>Croquete de Carne</h3>
+                  <p>Crab / Potatoes / Rice</p>
+                </div>
+              </li>
+            </ul>
+          </div>
+          <div class="col-md-6">
+            <ul class="menus">
+              <li>
+                <figure class="image"><img src="img/img_square_5.jpg" alt="Free Bootstrap Template by uicookies.com"></figure>
+                <div class="text">
+                  <span class="price">R$ 8,00</span>
+                  <h3>Coxinhas de Frango</h3>
+                  <p>Crab / Potatoes / Rice</p>
+                </div>
+              </li>
+              <li>
+                <figure class="image"><img src="img/img_square_1.jpg" alt="Free Bootstrap Template by uicookies.com"></figure>
+                <div class="text">
+                  <span class="price">R$ 30.00</span>
+                  <h3>Pudim Light</h3>
+                  <p>Crab / Potatoes / Rice</p>
+                </div>
+              </li>
+              <li>
+                <figure class="image"><img src="img/img_square_2.jpg" alt="Free Bootstrap Template by uicookies.com"></figure>
+                <div class="text">
+                  <span class="price">R$ 10,00</span>
+                  <h3>Mousse de Morango</h3>
+                  <p>Crab / Potatoes / Rice</p>
+                </div>
+              </li>
+              <li>
+                <figure class="image"><img src="img/img_square_3.jpg" alt="Free Bootstrap Template by uicookies.com"></figure>
+                <div class="text">
+                  <span class="price">R$ 10,00</span>
+                  <h3>Mousse de Maracujá</h3>
+                  <p>Crab / Potatoes / Rice</p>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="probootstrap-section probootstrap-bg-white" data-section="contact">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-5 text-center probootstrap-animate">
+            <div class="probootstrap-heading dark">
+              <h1 class="primary-heading">Contato</h1>
+              <h3 class="secondary-heading">Fale Conosco</h3>
+            </div>
+            <p>Aqui você pode: Enviar suas sujestões, tirar dúvidas, mandar depoimentos e até mesmo fazer seus pedidos! <br>
+              <strong>Não perca tempo, mande-nos sua mensagem!</strong></p>
+          </div>
+          <div class="col-md-6 col-md-push-1 probootstrap-animate">
+            <form method="post" class="probootstrap-form">
+              <div class="form-group">
+                <label for="c_name">Qual seu nome?</label>
+                <div class="form-field">
+                  <input type="text" id="c_name" class="form-control">
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="c_email">e-mail</label>
+                <div class="form-field">
+                  <input type="text" id="c_email" class="form-control">
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="c_message">Diga-nos alguma coisa</label>
+                <div class="form-field">
+                  <textarea name="c_message" id="c_message" cols="30" rows="10" class="form-control"></textarea>
+                </div>
+              </div>
+              <div class="form-group">
+                <input type="submit" name="c_submit" id="c_submit" value="Enviar Mensagem" class="btn btn-primary btn-lg">
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <?php require_once('components/footer.html')?>
+    <?php require_once('components/copyright.html')?>
+    
+
+    <script src="js/scripts.min.js"></script>
+    <script src="js/custom.min.js"></script>
+
+  </body>
+</html>
